f9e32b750d6e20288e153a936e73edc1889cb24f
y_mc <momo.toure27@gmail.com>
2025-08-10 02:35:39 +0200

    Package update

---
+++ protocols/src/index.ts
@@ -0,0 +1,15 @@
+/**
+ * Protocols package exports
+ */
+
+// A2A Protocol exports
+export { A2AProtocol } from './a2a/A2AProtocol';
+export { A2AAgentRegistry } from './a2a/A2AAgentRegistry';
+export { MessageRouter } from './a2a/MessageRouter';
+export { SecurityManager } from './a2a/SecurityManager';
+export { A2AAgentServer } from './a2a/A2AAgentServer';
+export { A2AClient } from './a2a/A2AClient';
+export { AgentExecutor } from './a2a/AgentExecutor';
+
+// Export all types
+export * from './a2a/types';
